fdd0b6605d230a53c06d9551a1b95f2b6fa134d7
Kitwana Ezechiel <rodriguekitweze@gmail.com>
2024-05-13 22:50:18 +0000

    Feat: Finish up the code

--- anime_RestAPI/settings.py
+++ anime_RestAPI/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     
     # Core apps
     'users',
+    'core',
 ]
 
 MIDDLEWARE = [
---
+++ core/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from core.models import CustomUser, Payment, Product, Cart, CartItem, ProductReview, Checkout, MerchantProfile, Notification
+
+
+# Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(Payment)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Product)
+admin.site.register(ProductReview)
+admin.site.register(MerchantProfile)
+admin.site.register(Checkout)
+admin.site.register(Notification)
